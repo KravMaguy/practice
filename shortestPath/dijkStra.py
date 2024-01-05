350b4789b11b7c9d0456c73cce019600afd1c861
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2024-01-05 15:33:20 -0500

    dijkstras as described: https://www.youtube.com/watch?v=bZkzH5x0SKU

--- shortestPath/dijkStra.py
+++ shortestPath/dijkStra.py
@@ -1,29 +1,38 @@
+# my_graph = {
+#     'A': [('B', 2), ('D', 8)],
+#     'B': [('A', 2), ('D', 5), ('E', 6)],
+#     'C': [('E', 9), ('F', 3)],
+#     'D': [('A', 8), ('B', 5), ('E', 3), ('F', 2)],
+#     'E': [('B', 6), ('D', 3), ('F', 1), ('C', 9)],
+#     'F': [('D', 2), ('E', 1), ('C', 3)]
+# }
+
 my_graph = {
-    'A': [('B', 2), ('D', 8)],
-    'B': [('D', 5), ('E', 6)],
-    'C': [('E', 9), ('F', 3)],
-    'D': [('A', 8), ('B', 5), ('E', 3), ('F', 2)],
-    'E': [('B', 6), ('D', 3), ('F', 1), ('C', 9)],
-    'F': [('D', 2), ('E', 1), ('C', 3)]
+    'A': [('B', 3), ('C', 2)],
+    'B': [('A', 3), ('C', 10), ('D', 1)],
+    'C': [('A', 2), ('B', 10)],
+    'D': [('B', 1)],
 }
 
 
-def shortest_path(graph, start):
-
+def shortest_path(graph, start, target):
     unvisited = list(graph)
     distances = {node: 0 if node == start else float('inf') for node in graph}
-    paths = {node: [] for node in graph}
-    paths[start].append(start)
-    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
+    mapping = {node: start if node == start else None for node in graph}
     while unvisited:
         current = min(unvisited, key=distances.get)
-        unvisited.remove(current)
         for node, distance in graph[current]:
-            # print(node, distance)
-            # print(graph[current])
-            if distances[current]+distance < distances[node]:
-                distances[node] = distances[current]+distance
-        print('dist: ', distances)
+            if distance + distances[current] < distances[node]:
+                distances[node] = distance + distances[current]
+                mapping[node] = current
+        unvisited.remove(current)
+    distance = distances[target]
+    node = target
+    paths = [node]
+    while node is not start:
+        paths = [mapping[node]] + paths
+        node = mapping[node]
+    print(' -> '.join(paths) + ' dist: '+str(distance))
 
 
-shortest_path(my_graph, 'A')
+shortest_path(my_graph, 'A', 'C')
---
+++ shortestPath/readme.md
@@ -0,0 +1,7 @@
+https://www.youtube.com/watch?v=bZkzH5x0SKU
+
+how is the code different than what is shown here:
+
+https://github.com/freeCodeCamp/freeCodeCamp/blob/main/curriculum/challenges/english/07-scientific-computing-with-python/learn-algorithm-design-by-building-a-shortest-path-algorithm/6559da93115de78dbbdc7ba3.md
+
+what would be the need to keep track of previously visited nodes
