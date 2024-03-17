6262dff83c251ffb54a85c5b821658fa4708678a
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2024-03-17 14:15:51 -0400

    just p will return starting address

--- executables/func_basic.c
+++ executables/func_basic.c
@@ -3,11 +3,22 @@ int add(int a, int b)
 {
     return a + b;
 }
+// int (*Func)(int, int);
 
 int main()
 {
+    int c;
     int (*p)(int, int);
     p = &add;
     printf("%d\n", p);
+    // dereference and execute
     printf("%d\n", (*p)(2, 3));
+    c = (*p)(2, 3);
+    printf("%d\n", c);
+
+    int added = p(4, 5);
+    printf("%d\n", added);
+
+    // review
+    // int *P(int, int);
 }
